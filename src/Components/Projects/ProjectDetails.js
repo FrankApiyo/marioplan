8dda02f2a0f38cb1ef7a38e89dcc6f7a7deed635
FrankApiyo <franklineapiyo@gmail.com>
2019-10-22 18:24:57 +0300

    Show project details when Project is clicked from ProjectList

--- src/Components/Projects/ProjectDetails.js
+++ src/Components/Projects/ProjectDetails.js
@@ -1,25 +1,47 @@
 import React from 'react'
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
-export default function ProjectDetails(props) {
-    const id = props.match.params.id;
-    return (
-        <div className="container section project-details">
-            <div className="card z-depth-o">
-                <span className="card-title">
-                    Project Title-{id}
-                </span>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia hic placeat eos, rem modi animi amet cumque perspiciatis dolorum rerum nostrum quisquam! Beatae nisi, dolorum quisquam blanditiis recusandae similique? Veritatis?
-                </p>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>
-                        Posted by the net ninja
+function ProjectDetails(props) {
+    const { project } = props;
+    console.log(project ? new Date(project.createdAt.seconds): "nothing")
+    if (project) {
+        return (
+            <div className="container section project-details">
+                <div className="card z-depth-o">
+                    <span className="card-title">
+                        {project.title}
+                    </span>
+                    <p>{project.content}</p>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>
+                            Posted by {project.authorFirstName} {project.authorLastName}
                     </div>
-                    <div>
-                        2nd september, am
+                        <div>
+                            {project.createdAt ? new Date(project.createdAt.seconds).toString() : "I don't know when this was created"}
+                    </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    )
-}
+            </div>)
+    }else{
+        return 'test'
+    }
+    }
+
+    const mapStateToProps = (state, ownProps) => {
+
+        const id = ownProps.match.params.id;
+        const projects = state.firestore.data.projects;
+        const project = projects ? projects[id] : null;
+        return {
+            project
+        }
+    }
+
+    export default compose(
+        connect(mapStateToProps),
+        firestoreConnect([
+            { collection: 'projects' }
+        ])
+    )(ProjectDetails);
